c1f9773b487520852bff6725257fe5b7e1a08956
Hani <Agguinihani.off@gmail.com>
2024-04-07 17:24:35 +0200

    affichage V5.0

--- src/main/java/com/example/calendrier_ceri/Parseur.java
+++ src/main/java/com/example/calendrier_ceri/Parseur.java
@@ -164,7 +164,7 @@ public class Parseur {
     }
 
     public static void main(String[] args) {
-        String filePath = "m1_general.ics";
+        String filePath = "amphi_ada.ics";
         List<CalendarActivity> activities = parceFichier(filePath);
 
         for (CalendarActivity evenement : activities) {
